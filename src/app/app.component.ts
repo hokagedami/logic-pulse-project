c9e0ae5153901866d8edbeffdcf9ef28e2e7b66f
Michael Akinyemi <36125244+hokagedami@users.noreply.github.com>
2024-09-21 17:16:26 +0100

    add warning for small screen; show canvas task correct or incorrect after answer submission (#16)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
 import {FooterComponent} from "./components/footer/footer.component";
 import {CookieService} from "ngx-cookie-service";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'logic-gate';
+export class AppComponent implements OnInit {
 
-  // clear cookies on page load
+  title = 'logic-gate';
+  isMobile: boolean = false;
+
   constructor(cookieService: CookieService) {
     cookieService.deleteAll();
   }
+  ngOnInit() {
+   this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768; // Adjust the width as needed
+  }
+
 }
